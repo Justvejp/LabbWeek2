5fad79b506c61502ef84117cf72b779e884557e3
Gustav Olsson <70860287+Justvejp@users.noreply.github.com>
2020-09-21 18:01:55 +0200

    Add files via upload

---
+++ FileCrawler2.0/src/com/company/Main.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import java.util.Scanner;
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File startingFolder = new File("C:\\Users\\Gurra\\DokumentsGitub\\FileCrawler2.0");
+        Scanner key = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Welcome to FileCrawler!");
+            System.out.print("Enter a keyword: ");
+            String keyword = key.next();
+
+            checkIfFile(startingFolder, keyword);
+        }
+    }
+
+    public static void checkIfFile(File startingFolder, String keyword) {
+
+        if (startingFolder.isFile()) {
+            scanFile(startingFolder, keyword);
+
+        } else if (startingFolder.isDirectory()) {
+
+            try {
+                File[] folderContents = startingFolder.listFiles();
+                assert folderContents != null;
+                for (File fileList : folderContents) {
+                    checkIfFile(fileList, keyword);
+                }
+
+            } catch (Exception e) {
+                System.err.println("Error: " + startingFolder.getAbsolutePath());
+
+            }
+
+        } else {
+            System.err.println("Error: " + startingFolder.getAbsolutePath());
+
+        }
+    }
+
+    public static void scanFile(File startingFolder, String keyword) {
+
+        try {
+            Scanner sc = new Scanner(startingFolder);
+
+            while (sc.hasNextLine()) {
+               String s = sc.nextLine();
+
+                if (s.contains(keyword)) {
+                    System.out.println(startingFolder.getCanonicalPath());
+                }
+
+            } sc.close();
+
+        } catch (Exception e) {
+            System.err.println("Error: " + startingFolder.getAbsolutePath());
+
+        }
+    }       // av Gustav och Cuneyt
+}
